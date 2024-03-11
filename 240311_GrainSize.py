bda7d665f3ad23af49094a04348022e315d39ee2
moony <91704559+repositoryfiles@users.noreply.github.com>
2024-03-11 17:20:10 +0900

    240311

--- 240311_GrainSize.py
+++ 240311_GrainSize.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 #ライブラリのインポート
 import cv2
-import tkinter as tk
-import tkinter.filedialog as filedialog
-import tkinter.simpledialog as simpledialog
+import tkinter
+from tkinter import filedialog
 import numpy as np
 import math
 
 #変数の初期化
 pt = {}
+testline_pt = {}
 m = 0
 n = 0
 AddPoints = 0
@@ -16,12 +16,14 @@ DeletePoints = 0
 
 # PictureWidthとMagnificationは組織画像に合致した値に設定すること！
 # 下は、画像を幅142mmで表示すると、倍率1000倍の組織画像になるという設定である
-#PictureWidth = 142 #画像の幅（単位：mm）
-#Magnification = 1000 #撮影倍率
-PictureWidth = 100 #画像の幅（単位：mm）
-Magnification = 100 #撮影倍率
+PictureWidth = 142 #画像の幅（単位：mm）
+Magnification = 1000 #撮影倍率
+miniGraSize=10/PictureWidth #（認識させる最小サイズ）/（画像の幅）
 Width=640#表示させる画像の幅（高さは元画像から計算）
-AnalysisPictureHeight = 140
+
+Radius1 = 79.58/2/PictureWidth
+Radius2 = 53.05/2/PictureWidth
+Radius3 = 26.53/2/PictureWidth
 
 #マウスの操作があるとき呼ばれる関数
 def callback(event, x, y, flags, param):
@@ -30,13 +32,13 @@ def callback(event, x, y, flags, param):
     #マウスの左ボタンがクリックされたとき
     if event == cv2.EVENT_LBUTTONDOWN:
 
-
-
+        x_value = x-center_x
+        y_value = y-center_y
 
         #三つのほぼ円周上を左クリックしたときのみ点を追加
-        #if abs(math.sqrt(x_value**2 + y_value**2) - int(Radius1*Width)) < 2 \
-        #or abs(math.sqrt(x_value**2 + y_value**2) - int(Radius2*Width)) < 2 \
-        #or abs(math.sqrt(x_value**2 + y_value**2) - int(Radius3*Width)) < 2:
+        if abs(math.sqrt(x_value**2 + y_value**2) - int(Radius1*Width)) < 2 \
+        or abs(math.sqrt(x_value**2 + y_value**2) - int(Radius2*Width)) < 2 \
+        or abs(math.sqrt(x_value**2 + y_value**2) - int(Radius3*Width)) < 2:
             m = n
             pt[n] = (x, y)
             n = n + 1
@@ -66,10 +68,9 @@ def DrawFigure():
     center_x = int(Width / 2)
     center_y = int(Height / 2)
 
-    cv2.line(copy_img_color, (int((PictureWidth/2 - 60)/PictureWidth*Width), int((70-50)/PictureHeight*Height)), (int((PictureWidth/2 - 60)/PictureWidth*Width), int((70-50 + 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
-    cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((130)/PictureHeight*Height)), (int((PictureWidth/2 - 50 + 100)/PictureWidth*Width), int((130)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
-    cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((70-50)/PictureHeight*Height)), (int((PictureWidth/2 - 50 + 100)/PictureWidth*Width), int((70-50 + 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
-    cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((70+50)/PictureHeight*Height)), (int((PictureWidth/2 - 50 + 100)/PictureWidth*Width), int((70+50 - 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
+    cv2.circle(copy_img_color, (center_x, center_y), int(Radius1*Width), (0,0,255), thickness=2) #円描画
+    cv2.circle(copy_img_color, (center_x, center_y), int(Radius2*Width), (0,0,255), thickness=2) #円描画
+    cv2.circle(copy_img_color, (center_x, center_y), int(Radius3*Width), (0,0,255), thickness=2) #円描画
 
     for i in pt:
         cv2.circle(copy_img_color, pt[i], int(Width/100), (255,0,0), thickness=2) #円描画
@@ -86,37 +87,50 @@ def DrawFigure():
     cv2.namedWindow("Result", 16) #組織画像のwindow内で右クリックのメニューを非表示にする
     cv2.imshow("Result", copy_img_color)
 
+def generate_testline_point():
+    global testline_pt
+    center_x = int(Width / 2)
+    center_y = int(Height / 2)
+    n = 0
+    for i in range(360):
+        theata_rad = i * math.pi/180
+        x1 = int(center_x + Radius1*Width * math.cos(theata_rad))
+        y1 = int(center_y + Radius1*Width * math.sin(theata_rad))
+        testline_pt[n] = (x1, y1)
+        n = n + 1
+        #print(n,x1,y1)
+
+    for i in range(360):
+        theata_rad = i * math.pi/180
+        x1 = int(center_x + Radius2*Width * math.cos(theata_rad))
+        y1 = int(center_y + Radius2*Width * math.sin(theata_rad))
+        testline_pt[n] = (x1, y1)
+        n = n + 1
+        #print(n,x1,y1)
+
+    for i in range(360):
+        theata_rad = i * math.pi/180
+        x1 = int(center_x + Radius3*Width * math.cos(theata_rad))
+        y1 = int(center_y + Radius3*Width * math.sin(theata_rad))
+        testline_pt[n] = (x1, y1)
+        n = n + 1
+        #print(n,x1,y1)
+
+
 #ファイル選択（c:\Dataの拡張子jpgを開く場合）
-tk.Tk().withdraw()
+root=tkinter.Tk()
+root.withdraw()
 fTyp = [("jpg", "*.jpg"), ("BMP", "*.bmp"), ("png", "*.png"), ("tiff", "*.tif")] #画像の種類を選択
 iDir='C:/Data'
-fname=filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir) 
-
-#ダイアログでPictureWidthとMagnificationを入力
-tk.Tk().withdraw()
-PhotoMagnification = simpledialog.askstring('画像の倍率入力', '画像の倍率を入力してください', initialvalue="1000")
-tk.Tk().withdraw()
-PhotoPictureWidth = simpledialog.askstring('画像の幅入力', f'{PhotoMagnification}倍で表示するための画像の幅を入力してください', initialvalue="142")
-PhotoMagnification = int(PhotoMagnification)
-PhotoPictureWidth = int(PhotoPictureWidth)
-print(f'Photo Magnification = {PhotoMagnification}, type = {type(PhotoMagnification)}, Photo Picture Width = {PictureWidth}, type = {type(PhotoPictureWidth)}')
+fname=filedialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
 
 #ファイル読み込み
 img_color= cv2.imread(fname) #画像ファイルのデータをimg_colorに代入
 img_gray = cv2.imread(fname, cv2.IMREAD_GRAYSCALE) #画像ファイルのデータをグレースケールでimg_grayに代入
+
 img_height, img_width = img_gray.shape #画像ファイルのサイズの取得
+
 Height=int(Width*img_height/img_width)
-
-#ここに、解析用の画像の幅と倍率の計算処理を入れる
-PhotoPictureHeight = PhotoPictureWidth * img_height/img_width
-Magnification = (AnalysisPictureHeight/PhotoPictureHeight)*PhotoMagnification
-PictureWidth = PhotoPictureWidth * Magnification / PhotoMagnification
-PictureHeight = PictureWidth * img_height/img_width
-
-miniGraSize=10/PictureWidth #（認識させる最小サイズ）/（画像の幅）
-#Radius1 = 79.58/2/PictureWidth
-#Radius2 = 53.05/2/PictureWidth
-#Radius3 = 26.53/2/PictureWidth
 
 img_color = cv2.resize(img_color, (Width, Height))
 img_gray = cv2.resize(img_gray, (Width, Height))
@@ -134,15 +148,17 @@ contours1 = [e for e in contours if int(Width * miniGraSize)  > int(cv2.minEnclo
 
 cv2.drawContours(img_gray_inv_binary, contours1, -1, (0, 0, 0), -1)
 
-center_x = int(Width / 2)
-center_y = int(Height / 2)
+
 
 Flag1 = 0
 Flag1_1 = 0
-for i in range(1000):
-    x1 = int((PictureWidth/2 - 60)/PictureWidth*Width)
-    y1 = int((70-50 + i / 10)/PictureHeight*Height)
-    kido = img_gray_inv_binary[y1, x1]
+
+generate_testline_point()
+
+print(len(testline_pt))
+
+for tp in testline_pt:
+    kido = img_gray_inv_binary[testline_pt[tp]]
     if kido == 255 :
         Flag1 = 1
         if Flag1 == 1 and Flag1_1 == 0:
@@ -155,59 +171,6 @@ for i in range(1000):
         continue
     Flag1_1 = Flag1
 
-Flag2 = 0
-Flag2_1 = 0
-for i in range(1000):
-    x1 = int((PictureWidth/2 - 50 + i / 10)/PictureWidth*Width)
-    y1 = int((130)/PictureHeight*Height)
-    kido = img_gray_inv_binary[y1, x1]
-    if kido == 255 :
-        Flag2 = 1
-        if Flag2 == 1 and Flag2_1 == 0:
-            m = n
-            pt[n] = (x1, y1)
-            n = n + 1
-    elif kido != 255 :
-        Flag2 = 0
-    else :
-        continue
-    Flag2_1 = Flag2
-
-Flag3 = 0
-Flag3_1 = 0
-for i in range(1000):
-    x1 = int((PictureWidth/2 - 50 + i / 10)/PictureWidth*Width)
-    y1 = int((70-50 + i / 10)/PictureHeight*Height)
-    kido = img_gray_inv_binary[y1, x1]
-    if kido == 255 :
-        Flag3 = 1
-        if Flag3 == 1 and Flag3_1 == 0:
-            m = n
-            pt[n] = (x1, y1)
-            n = n + 1
-    elif kido != 255 :
-        Flag3 = 0
-    else :
-        continue
-    Flag3_1 = Flag3
-
-Flag4 = 0
-Flag4_1 = 0
-for i in range(1000):
-    x1 = int((PictureWidth/2 - 50 + i / 10)/PictureWidth*Width)
-    y1 = int((70+50 - i / 10)/PictureHeight*Height)
-    kido = img_gray_inv_binary[y1, x1]
-    if kido == 255 :
-        Flag4 = 1
-        if Flag4 == 1 and Flag4_1 == 0:
-            m = n
-            pt[n] = (x1, y1)
-            n = n + 1
-    elif kido != 255 :
-        Flag4 = 0
-    else :
-        continue
-    Flag4_1 = Flag4
 
 DrawFigure()
 
