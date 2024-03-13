abd50ea5b39ecf7ed813172059e03ace761a975e
moony <91704559+moony-chan@users.noreply.github.com>
2024-03-14 06:16:43 +0900

    240314

--- GrainSize_2.py
+++ GrainSize_2.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-#ライブラリのインポート
+# ライブラリのインポート
 import cv2
 import tkinter
 from tkinter import filedialog
@@ -13,19 +13,17 @@ testline_pts = []
 n = 0
 AddPoints = 0
 DeletePoints = 0
+#Method = 'Circles' # 'Circles' または 'Lines' を指定
+Method = 'Lines' # 'Circles' または 'Lines' を指定
 
-# PictureWidthとMagnificationは組織画像に合致した値に設定すること！
-# 下は、画像を幅142mmで表示すると、倍率1000倍の組織画像になるという設定である
-
-Width=640#表示させる画像の幅（高さは元画像から計算）
-
-AnalysisPictureHeight = 140
+# PhotoPictureWidthとPhotoMagnificationは撮影環境により変わるため、組織画像に合致した値に設定すること！
+# 以下のPhotoPictureWidthとPhotoMagnificationは、画像を幅142mmで表示すると、倍率1000倍の組織画像になるという設定である
+PhotoPictureWidth = 142 #画像の幅（撮影倍率で表示時の画像の幅）
 PhotoMagnification = 1000 #撮影倍率
-PhotoPictureWidth = 142 #画像の幅
-PictureWidth = 100 #解析時の画像の幅（単位：mm）、初期値100
-Magnification = 500 #解析時の撮影倍率、初期値500
 
-
+# 次の二つの値は変えなくてよい
+Width=640 #画像のデフォルトの表示幅（高さは元画像から計算）、ディスプレイで見やすい大きさに設定
+AnalysisPictureHeight=140 #解析時の画像の高さ（解析時の倍率で表示したときの画像の高さ：単位mm）
 
 #マウスの左右ボタンがクリックされたときの処理
 def callback(event, x, y, flags, param):
@@ -36,9 +34,7 @@ def callback(event, x, y, flags, param):
         i = 0
         for testline_pt in testline_pts:
             #クリック位置(x,y)とtestline_ptsとの距離を求める
-            #distance = math.sqrt(math.pow((x - _pt[1]), 2) + math.pow((y - _pt[0]), 2))
-            #print(distance)
-            if math.sqrt(math.pow((x - testline_pt[1]), 2) + math.pow((y - testline_pt[0]), 2)) < 2:
+            if math.sqrt(math.pow((x - testline_pt[1]), 2) + math.pow((y - testline_pt[0]), 2)) < 4:
                 pts.append([y, x])
                 n = n + 1
                 AddPoints = AddPoints + 1
@@ -48,7 +44,7 @@ def callback(event, x, y, flags, param):
     if event == cv2.EVENT_RBUTTONDOWN:
         flag = 0
         i = 0
-        #pt[]からクリック位置に近い点を探す
+        #ptsからクリック位置に近い点を探す
         for pt in pts:
             if abs(pt[0] - y) < 5 and abs(pt[1] - x) < 5:
                 point = i
@@ -70,22 +66,31 @@ def DrawFigure():
     center_x = int(Width / 2)
     center_y = int(Height / 2)
 
-    cv2.circle(copy_img_color, (center_x, center_y), int(Radius1*Width), (0,0,255), thickness=2) #円描画
-    cv2.circle(copy_img_color, (center_x, center_y), int(Radius2*Width), (0,0,255), thickness=2) #円描画
-    cv2.circle(copy_img_color, (center_x, center_y), int(Radius3*Width), (0,0,255), thickness=2) #円描画
+    if Method == 'Circles':
+        cv2.circle(copy_img_color, (center_x, center_y), int(Radius1*Width), (0,0,255), thickness=2) #円描画
+        cv2.circle(copy_img_color, (center_x, center_y), int(Radius2*Width), (0,0,255), thickness=2) #円描画
+        cv2.circle(copy_img_color, (center_x, center_y), int(Radius3*Width), (0,0,255), thickness=2) #円描画
+
+    elif Method == 'Lines':
+        cv2.line(copy_img_color, (int((PictureWidth/2 - 60)/PictureWidth*Width), int((20)/PictureHeight*Height)), (int((PictureWidth/2 - 60)/PictureWidth*Width), int((70-50 + 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
+        cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((130)/PictureHeight*Height)), (int((PictureWidth/2 + 50)/PictureWidth*Width), int((130)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
+        cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((20)/PictureHeight*Height)), (int((PictureWidth/2 + 50)/PictureWidth*Width), int((70-50 + 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
+        cv2.line(copy_img_color, (int((PictureWidth/2 - 50)/PictureWidth*Width), int((120)/PictureHeight*Height)), (int((PictureWidth/2 + 50)/PictureWidth*Width), int((70+50 - 100)/PictureHeight*Height)), (0,0,255), thickness=2, lineType=cv2.LINE_8, shift=0)
 
     for _pt in pts:
         cv2.circle(copy_img_color, (_pt[1], _pt[0]), int(Width/100), (255,0,0), thickness=2) #円描画
 
     points_num = len(pts)
-    #cv2.putText(copy_img_color, "Number of grain boundary : " + str(len(pt)) , (int(Width/20), int(Height/15)), cv2.FONT_HERSHEY_PLAIN, Height/400, (255, 255, 255), 2, cv2.LINE_AA)
 
     point_num_per_1mm = points_num / (500 / Magnification)
 	# G0551の式A.11と式A.14の関係を使用（A.11からG(ASTM)を求め、それA.14を使ってGに変換）
     grain_number = -3.3335 + 6.6439 * math.log10(point_num_per_1mm)
+    print(f'\nMagnification (for picture height = {AnalysisPictureHeight:.0f} mm): {Magnification:.0f}')
     print(f'Number of grain boundaries : {points_num}')
-    print(f'Number of grain boundaries per 1 mm : {point_num_per_1mm}')
+    print(f'Number of grain boundaries per 1 mm : {point_num_per_1mm:.1f}')
     print(f'Apparent grain size : {grain_number :.1f}')
+    #cv2.putText(copy_img_color, "Apparent grain size : " + f'{grain_number :.1f}' , (int(Width/20), int(Height/15)), cv2.FONT_HERSHEY_PLAIN, Height/400, (0, 0, 255), 2, cv2.LINE_AA)
+
     cv2.namedWindow("Result", 16) #組織画像のwindow内で右クリックのメニューを非表示にする
     cv2.imshow("Result", copy_img_color)
 
@@ -93,22 +98,49 @@ def DrawFigure():
 def generate_testline_point():
 
     global testline_pts
-    center_x = int(Width / 2)
-    center_y = int(Height / 2)
+    if Method == 'Circles':
+        center_x = int(Width / 2)
+        center_y = int(Height / 2)
 
-    for i in range(360):
-        theata_rad = i * math.pi/180
-        x1 = int(center_x + Radius1*Width * math.cos(theata_rad))
-        y1 = int(center_y + Radius1*Width * math.sin(theata_rad))
-        testline_pts.append([y1, x1])
+        for i in range(180):
+            theata_rad = 2 * i * math.pi/180
+            x1 = int(center_x + Radius1*Width * math.cos(theata_rad))
+            y1 = int(center_y + Radius1*Width * math.sin(theata_rad))
+            testline_pts.append([y1, x1])
 
-        x2 = int(center_x + Radius2*Width * math.cos(theata_rad))
-        y2 = int(center_y + Radius2*Width * math.sin(theata_rad))
-        testline_pts.append([y2, x2])
+        for i in range(120):
+            theata_rad = 3 * i * math.pi/180
+            x2 = int(center_x + Radius2*Width * math.cos(theata_rad))
+            y2 = int(center_y + Radius2*Width * math.sin(theata_rad))
+            testline_pts.append([y2, x2])
 
-        x3 = int(center_x + Radius3*Width * math.cos(theata_rad))
-        y3 = int(center_y + Radius3*Width * math.sin(theata_rad))
-        testline_pts.append([y3, x3])
+        for i in range(90):
+            theata_rad = 4 * i * math.pi/180
+            x3 = int(center_x + Radius3*Width * math.cos(theata_rad))
+            y3 = int(center_y + Radius3*Width * math.sin(theata_rad))
+            testline_pts.append([y3, x3])
+
+    elif Method == 'Lines':
+        for i in range(100):
+            x1 = int((PictureWidth/2 - 60)/PictureWidth*Width)
+            y1 = int((20 + i)/PictureHeight*Height)
+            testline_pts.append([y1, x1])
+
+        for i in range(100):
+            x2 = int((PictureWidth/2 - 50 + i)/PictureWidth*Width)
+            y2 = int((130)/PictureHeight*Height)
+            testline_pts.append([y2, x2])
+
+        for i in range(100):
+            x3 = int((PictureWidth/2 - 50 + i)/PictureWidth*Width)
+            y3 = int((20 + i)/PictureHeight*Height)
+            testline_pts.append([y3, x3])
+
+        for i in range(100):
+            x4 = int((PictureWidth/2 - 50 + i)/PictureWidth*Width)
+            y4 = int((120 - i)/PictureHeight*Height)
+            testline_pts.append([y4, x4])
+
 
 #ファイル選択（c:\Dataの拡張子jpgを開く場合）
 root=tkinter.Tk()
@@ -125,18 +157,21 @@ img_height, img_width = img_gray.shape #画像ファイルのサイズの取得
 
 Height=int(Width*img_height/img_width)
 
+img_color = cv2.resize(img_color, (Width, Height))
+img_gray = cv2.resize(img_gray, (Width, Height))
+
 #ここに、解析用の画像の幅と倍率の計算処理を入れる
 PhotoPictureHeight = PhotoPictureWidth * img_height/img_width
 Magnification = (AnalysisPictureHeight/PhotoPictureHeight)*PhotoMagnification
 PictureWidth = PhotoPictureWidth * Magnification / PhotoMagnification
 PictureHeight = PictureWidth * img_height/img_width
 miniGraSize=10/PictureWidth #（認識させる最小サイズ）/（画像の幅）
+
+#3個の同心円の半径を計算
 Radius1 = 79.58/2/PictureWidth
 Radius2 = 53.05/2/PictureWidth
 Radius3 = 26.53/2/PictureWidth
-
-img_color = cv2.resize(img_color, (Width, Height))
-img_gray = cv2.resize(img_gray, (Width, Height))
+miniGraSize=10/PictureWidth #（認識させる最小サイズ）/（画像の幅）
 
 #リサイズ後のimg_colorのクローン
 copy_img_color = img_color.copy()
@@ -151,27 +186,23 @@ contours1 = [e for e in contours if int(Width * miniGraSize)  > int(cv2.minEnclo
 
 cv2.drawContours(img_gray_inv_binary, contours1, -1, (0, 0, 0), -1)
 
-Flag1 = 0
-Flag1_1 = 0
-
 # 試験線の座標値を抽出
 generate_testline_point()
 
 #　試験線の座標値上の輝度値が255の座標を粒界として検出
+Flag1 = 0
+Flag2 = 0
 for testline_pt in testline_pts:
     y1 = testline_pt[0]
     x1 = testline_pt[1]
-    print(x1,y1)
     kido = img_gray_inv_binary[y1, x1]
     if kido == 255 :
         Flag1 = 1
-        if Flag1 == 1 and Flag1_1 == 0:
+        if Flag1 == 1 and Flag2 == 0:
             pts.append(testline_pt)
-    elif kido != 255 :
+    else:
         Flag1 = 0
-    else :
-        continue
-    Flag1_1 = Flag1
+    Flag2 = Flag1
 
 # 試験線と粒界位置を描画
 DrawFigure()
@@ -191,3 +222,4 @@ src = fname
 idx = src.rfind(r".")
 result_filename = (src[:idx] + "_result." + src[idx + 1 :])
 cv2.imwrite(result_filename, copy_img_color)
+
